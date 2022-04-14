6c16bf20f4a490d810bad9b32055c56661a06f40
santiMoreno1989 <santiagom@gmail.com>
2022-04-14 14:33:29 -0300

    Nuevo metodo GetByCondicion, permite filtrar un genero por su nombre.

--- ApiPreAceleracionAlkemy/Services/GeneroService.cs
+++ ApiPreAceleracionAlkemy/Services/GeneroService.cs
@@ -1,6 +1,8 @@
 ﻿using ApiPreAceleracionAlkemy.Entities;
+using ApiPreAceleracionAlkemy.Filter;
 using ApiPreAceleracionAlkemy.Interfaces;
 using ApiPreAceleracionAlkemy.Repositories;
+using ApiPreAceleracionAlkemy.ViewModel.GeneroView;
 using Microsoft.AspNetCore.Http;
 using System;
 using System.Collections.Generic;
@@ -12,7 +14,7 @@ namespace ApiPreAceleracionAlkemy.Services
 {
     public class GeneroService : IGeneroService
     {
-        
+
         private readonly IUnitOfWork _unitOfWork;
 
         public GeneroService(IUnitOfWork unitOfWork)
@@ -20,19 +22,14 @@ namespace ApiPreAceleracionAlkemy.Services
             _unitOfWork = unitOfWork;
         }
 
-        public GeneroService(Repository<Genero> repository)
-        {
-            _repository = repository;
-        }
-
         public async Task<Genero> Add(Genero entity)
         {
             return await _unitOfWork.generoRepository.Add(entity);
         }
 
-        public async Task  Delete(int id)
+        public async Task Delete(int id)
         {
-           await _unitOfWork.generoRepository.Delete(id);
+            await _unitOfWork.generoRepository.Delete(id);
         }
 
         public async Task<IEnumerable<Genero>> GetAll()
@@ -49,6 +46,17 @@ namespace ApiPreAceleracionAlkemy.Services
         public async Task<Genero> Update(Genero entity)
         {
             return await _unitOfWork.generoRepository.Update(entity);
+        }
+        public IEnumerable<GeneroGetViewModel> GetByCondition(string nombre) 
+        {
+            if (string.IsNullOrWhiteSpace(nombre) || nombre.Length < 3)
+                throw new NotFoundException($"No se han encontrado resultados con el nombre : {nombre}");
+
+            var internalGenero = _unitOfWork.generoRepository.FindByCondition(
+                n => n.Nombre.ToLower().Contains(nombre.ToLower())).Select(x=>(GeneroGetViewModel) x)
+                .OrderBy(n=> n.Nombre);
+            
+            return internalGenero;
         }
     }
 }
