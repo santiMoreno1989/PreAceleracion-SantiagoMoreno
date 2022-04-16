3291d6b3cd899de90b177bf222a7056d46a667d2
santiMoreno1989 <santiagom@gmail.com>
2022-04-15 22:34:22 -0300

    Validaciones con Data Annotations

--- ApiPreAceleracionAlkemy/ViewModel/PeliculaView/PeliculaPostViewModel.cs
+++ ApiPreAceleracionAlkemy/ViewModel/PeliculaView/PeliculaPostViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,9 +8,16 @@ namespace ApiPreAceleracionAlkemy.ViewModel
 {
     public class PeliculaPostViewModel
     {
+        [Display(Name ="Imagen")]
         public byte[] Imagen { get; set; }
+        [MaxLength(50, ErrorMessage ="Ha superado el maximo de 50 caracteres.")]
+        [Display(Name ="Titulo")]
+        [Required(ErrorMessage ="Este campo es requerido.")]
         public string Titulo { get; set; }
         public DateTime FechaCreacion { get; set; }
+        
+        [Display(Name ="Calificacion")]
+        [Range(1,5,ErrorMessage ="ERROR : La calificacion debe estar entre 1 y 5.")]
         public short Calificacion { get; set; }
     }
 }
